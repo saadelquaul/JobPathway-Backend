4490fb3ede51ed3edd36ad2d25a58c245ea12def
Saad_elquaul <saadelquaul@gmail.com>
2026-01-05 12:09:18 +0100

    first commit

---
+++ src/main/java/com/job/JobPathway/JobPathwayApplication.java
@@ -0,0 +1,13 @@
+package com.job.JobPathway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPathwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPathwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/job/JobPathway/JobPathwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job.JobPathway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPathwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
