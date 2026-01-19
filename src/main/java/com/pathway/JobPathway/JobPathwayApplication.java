fcfdb4cbcd5c26208b2e9568eadb1ef474133856
SaadElQuaul <saadelquaul@gmail.com>
2026-01-19 10:33:54 +0100

    Initializing the project with the structure directories

---
+++ src/main/java/com/pathway/JobPathway/JobPathwayApplication.java
@@ -0,0 +1,13 @@
+package com.pathway.JobPathway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPathwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPathwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pathway/JobPathway/JobPathwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pathway.JobPathway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPathwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
