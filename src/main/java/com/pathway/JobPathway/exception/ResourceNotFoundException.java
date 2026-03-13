122ca19ee30bdaf22032cc332bb05595fb71b2ad
SaadElQuaul <saadelquaul@gmail.com>
2026-03-13 15:25:41 +0000

    feat: add ResourceNotFoundException for handling resource not found errors

---
+++ src/main/java/com/pathway/JobPathway/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.pathway.JobPathway.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
