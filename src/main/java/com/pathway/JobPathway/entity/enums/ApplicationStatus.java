ccbcf7e9c7b582a3da6fbed0185f47258476b891
SaadElQuaul <saadelquaul@gmail.com>
2026-03-25 10:25:49 +0100

    feat: add IN_REVIEW status to ApplicationStatus enum; create NotificationType enum for notification management

--- src/main/java/com/pathway/JobPathway/entity/enums/ApplicationStatus.java
+++ src/main/java/com/pathway/JobPathway/entity/enums/ApplicationStatus.java
@@ -2,6 +2,7 @@ package com.pathway.JobPathway.entity.enums;
 
 public enum ApplicationStatus {
     PENDING,
+    IN_REVIEW,
     REJECTED,
     MEETING_SCHEDULED,
     APPROVED
---
+++ src/main/java/com/pathway/JobPathway/entity/enums/NotificationType.java
@@ -0,0 +1,13 @@
+package com.pathway.JobPathway.entity.enums;
+
+public enum NotificationType {
+    // Admin notifications
+    NEW_APPLICATION,
+
+    // Candidate notifications
+    APPLICATION_UNDER_REVIEW,
+    MEETING_SCHEDULED,
+    APPLICATION_REJECTED,
+    APPLICATION_APPROVED,
+    APPLICATION_STATUS_CHANGED
+}
