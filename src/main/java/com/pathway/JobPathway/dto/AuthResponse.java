1bfc7b6a3397dff65528204dcf37092046c89b8e
SaadElQuaul <saadelquaul@gmail.com>
2026-03-26 11:09:23 +0100

    feat: add id field to AuthResponse DTO

--- src/main/java/com/pathway/JobPathway/dto/AuthResponse.java
+++ src/main/java/com/pathway/JobPathway/dto/AuthResponse.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthResponse {
+    private Long id;
     private String token;
     private String email;
     private String name;
